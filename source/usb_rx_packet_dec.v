602ccc0d4b8883257050d8e055c3356f6567ee76
YANG <yang@localhost.localdomain>
2020-01-21 18:24:20 +0900

    agile

--- source/usb_rx_packet_dec.v
+++ source/usb_rx_packet_dec.v
@@ -1,7 +1,7 @@
 // --------------------------------------------------------
 // File Name   : usb_phy.v
 // Description : usb phy controller
-//               recognize(rx)/generate(tx) Signal J/K/SE0/SE1 (SE:Single-Ended)
+//               recognize Signal J/K/SE0/SE1 (SE:Single-Ended)
 //                   D+   D-
 //               J : L    H  (Low Speed) (Full Speed is inverse) idle line state
 //               K : H    L  (Low Speed) (Full Speed is inverse) inverse of J
@@ -20,30 +20,25 @@ module usb_phy #(
 	input  wire                      rst_ni,
 	input  wire                      clk_i,        // 24MHz
 
-	// for physical line
-	input  wire                      usb_rx_dp_i, // weak pull-down(not allowed usb spec)
-	input  wire                      usb_rx_dm_i, // weak pull-down(not allowed usb spec)
+	// from usb_phy
+	input  wire                      rx_packet_st_i, // 1clk pulse after SYNC-detected
+	input  wire                      rx_packet_ed_i, // 1clk pulse after EOP-detected
 
-	output wire                      usb_tx_dp_o,
-	output wire                      usb_tx_dm_o,
-	output wire                      usb_tx_oe_o,
+	input  wire                      rx_data_i, // 
+	input  wire                      rx_den_i,  // 
 
-	output wire                      usb_dp_pull_up_en_o, // connect external pull-up resistor switch
-	output wire                      usb_dm_pull_up_en_o, // connect external pull-up resistor switch
-
-	input  wire                      usb_disconnect_i, // 1: set bus Hi-Z. 0: when reconnect, set J state after 2us SE0 state
+	input  wire                      rx_pid_error_i, // 1clk pulse after (pid(MSB4bit) != ~pid(LSB4bit))
 
 	// for internal RTL
-	output wire                      rx_packet_st_o, // 1clk pulse after SYNC-detected
-	output wire                      rx_packet_ed_o, // 1clk pulse after EOP-detected
-
-	output wire                      rx_data_o, // 
-	output wire                      rx_den_o,  // 
-
 	output wire                      rx_pid_error_o, // 1clk pulse after (pid(MSB4bit) != ~pid(LSB4bit))
+	output wire                      rx_crc_error_o, // 1clk pulse
 
+	input  wire                      tx_request_i, // 1clk pulse when idle state. keep TX(line status) until EOP.
 
-	input  wire                      tx_request_i  // 1clk pulse when idle state. keep TX(line status) until EOP.
+	output wire               [ 3:0] rx_state_o, // 0:idle,     F:UNKNOWN
+	output wire               [ 1:0] rx_packet_type_o, // 0:SOF / 1:TOKEN / 2:DATA / 3:Hand-Shake
+
+	output wire                      rx_crc_error_o  // 1clk pulse
 );
 
 
@@ -91,6 +86,10 @@ reg  [2*8-1:0] s_s_rx_symbol_window;
 wire           s_sync_det;
 wire           s_eop_det;
 
+wire           s_rx_data_dec;
+wire           s_rx_den_dec;
+
+
 // --------------------
 // BUS control
 // --------------------
@@ -104,6 +103,140 @@ end else
 	assign usb_dp_pull_up_en_o = 1'b0;
 	assign usb_dm_pull_up_en_o = 1'b1;
 end
+
+
+// --------------------
+// RX - FSM
+// --------------------
+// --------------------
+// FSM
+// --------------------
+// CLK EN
+assign s_rx_clk_en = (s_rx_clk_en_cntr == {P_RX_CLK_EN_CNTR_WIDTH{1'b0}}) ? 1'b1 : 1'b0;
+
+// CLK EN counter
+// Low Speed  : 1/16 @ 24MHz
+// Full Speed :  1/2 @ 24MHz
+always @(negedge rst_ni, posedge clk_i)
+begin
+	if(~rst_ni)
+		s_rx_clk_en_cntr <= {P_RX_CLK_EN_CNTR_WIDTH{1'b0};
+	else if(clk_i) begin
+		if (s_rx_chg_det) // clk recovery
+			s_rx_clk_en_cntr <= {P_RX_CLK_EN_CNTR_WIDTH{1'b0};
+		else
+			s_rx_clk_en_cntr <= s_rx_clk_en_cntr + 1'b1;
+	end
+end
+
+assign s_crlf  = wr_data_i == 8'h0A || wr_data_i == 8'h0D ? 1'b1 : 1'b0;
+assign s_space = wr_data_i == 8'h20  ? 1'b1 : 1'b0;
+
+localparam [3:0]  FSM_IDLE       = 4'h0; // SE0 >= 2us
+localparam [3:0]  FSM_SYNC       = 4'h1; // check KJKJKJKK (with wdt, goto UNKNOWN after timeout)
+localparam [3:0]  FSM_PID        = 4'h2; // check PID(goto UNKNOWN if 0000 or (MSB4bit == ~LSB4bit))
+localparam [3:0]  FSM_PAYLOAD    = 4'h3; // SE0 + SE0 + J
+localparam [3:0]  FSM_CRC        = 4'h4; // SE0 + SE0 + J
+localparam [3:0]  FSM_EOP        = 4'h5; // SE0 + SE0 + J
+
+assign s_sync_det = (s_s_rx_symbol_window == {3{K_STATE, J_STATE}, K_STATE, K_STATE}) ? 1'b1 : 1'b0;
+assign s_eop_det  = (s_s_rx_symbol_window[3*2-1:0] == {SE0_STATE, SE0_STATE, J_STATE}) ? 1'b1 : 1'b0;
+
+always @(negedge rst_ni, posedge clk_i)
+begin
+	if(~rst_ni)
+		s_next_state <= FSM_IDLE;
+	else if(clk_i) begin
+		if (wr_en_i) begin
+			case (s_state)
+				FSM_IDLE:
+					begin
+						s_state_wdt_cntr_en  <= 1'h0;
+						//s_state_wdt_cntr_max <= 4'h7;
+						if (s_rx_chg_det)
+							s_next_state <= FSM_SYNC;
+						else if(s_state_wdt_cntr == s_state_wdt_max)
+							s_next_state <= FSM_UNKNOWN;
+						else
+							s_next_state <= s_state;
+					end
+				FSM_SYNC:
+					begin
+						s_state_wdt_cntr_en  <= 1'h1;
+						s_state_wdt_cntr_max <= 4'h7;
+						if (s_sync_det)
+							s_next_state <= FSM_PID;
+						else if(s_state_wdt_cntr == s_state_wdt_max)
+							s_next_state <= FSM_UNKNOWN;
+						else
+							s_next_state <= s_state;
+					end
+				FSM_PID:
+					begin
+						s_state_wdt_cntr_en  <= 1'h1;
+						s_state_wdt_cntr_max <= 4'h7;
+						if (s_pid_error)
+							s_next_state <= FSM_UNKNOWN;
+						else if (s_pid_det) begin
+							case(s_packet_type)
+								PACKET_SOF	: s_next_state <= FSM_PAYLOAD;
+								PACKET_TOKEN  : s_next_state <= FSM_PAYLOAD;
+								PACKET_DATA   : s_next_state <= FSM_PAYLOAD;
+								PACKET_HNDSHK : s_next_state <= FSM_EOP;
+								// PACKET_SPLIT(USB2.0 only) is T.B.D.(when support USB2.0)
+								default	   : s_next_state <= FSM_UNKNOWN;
+							endcase
+						else if(s_state_wdt_cntr == s_state_wdt_max)
+							s_next_state <= FSM_UNKNOWN;
+						else
+							s_next_state <= s_state;
+					end
+				FSM_DATA_PAYLOAD:
+					begin
+						s_state_wdt_cntr_en  <= 1'h0; // disable(not use)
+						s_state_wdt_cntr_max <= 4'h0;
+						if (s_eop_det)
+							s_next_state <= FSM_IDLE; // skip FSM_EOP
+						//else if(s_state_wdt_cntr == s_state_wdt_max)
+						//	s_next_state <= FSM_UNKNOWN;
+						else
+							s_next_state <= s_state;
+					end
+				FSM_EOP:
+					begin
+						s_state_wdt_cntr_en  <= 1'h1;
+						s_state_wdt_cntr_max <= 4'h2; // necessery?
+						if (s_eop_det)
+							s_next_state <= FSM_IDLE;
+						else if(s_state_wdt_cntr == s_state_wdt_max)
+							s_next_state <= FSM_UNKNOWN;
+						else
+							s_next_state <= s_state;
+					end
+				FSM_UNKNOWN:
+					begin
+						s_state_wdt_cntr_en  <= 1'h1;
+						//s_state_wdt_cntr_max <= 4'h??; // need reset?
+						//if (???)
+						//	s_next_state <= FSM_IDLE;
+						//else
+						//	s_next_state <= s_state;
+					end
+				default:
+					s_next_state <= FSM_UNKNOWN;
+			endcase
+		end
+	end
+end
+
+always @(negedge rst_ni, posedge clk_i)
+begin
+	if(~rst_ni)
+		s_state <= FSM_IDLE;
+	else if(clk_i)
+		s_state <= s_next_state;
+end
+
 
 // --------------------
 // RX - generate CLK EN(Data EN)
@@ -224,8 +357,39 @@ begin
 	end
 end
 
-assign rx_data_o = s_rx_bit_window[0];
-assign rx_den_o  = s_rx_clk_en & s_rx_bit_stuff_den;
+assign s_rx_data_dec = s_rx_bit_window[0];
+assign s_rx_den_dec  = s_rx_clk_en & s_rx_bit_stuff_den;
+
+// --------------------
+// RX - Error check
+// --------------------
+// bit windows for decode NRZ-I(6bits) / CRC-5/16 and EOP(20bits)
+// check PID not same
+
+	output wire                      rx_pid_error_o, // 1clk pulse after (pid(MSB4bit) != ~pid(LSB4bit))
+
+always @(negedge rst_ni, posedge clk_i)
+begin
+	if(~rst_ni)
+		s_rx_bit_stuff_den <= 1'b1;
+	else if(clk_i) begin
+		if(s_rx_clk_en) begin
+			if(s_rx_bit_window == {6{1'b1}})
+				s_rx_bit_stuff_den <= 1'b0;
+			else
+				s_rx_bit_stuff_den <= 1'b1;
+		end
+	end
+end
+
+
+// RX - CRC cheker
+
+
+
+
+assign rx_irq_o  = s_rx_irq;
+assign rx_data_o = s_rx_data_1d;
 
 // --------------------
 // TX
